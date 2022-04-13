28ef5c10e1b6e56fdf3e29da7934220916a2b68a
Cy63rSi <20536891+evilsi@users.noreply.github.com>
2022-04-13 21:34:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,8 @@
 <html>
 <head>
   <title>Platform Check</title>
+  
+
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <script>
 
@@ -92,6 +94,12 @@
 </head> 
 <body>
 <h1> Rocket Telemetry Check</h1>
+  <div>
+  <form name="form" method="post" action="upload.php" enctype="multipart/form-data" >
+  <input type="file" name="my_file" /><br /><br />
+  <input type="submit" name="submit" value="Submit New Telemetry Data File"/>
+  </form>
+  </div>
 <div class="container">
   <div class="row">
     <div class="col-md-4">
@@ -107,6 +115,8 @@
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
 </body>
 </html>
